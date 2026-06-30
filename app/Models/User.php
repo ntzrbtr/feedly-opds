9eeadb5b9f2c04a856dabb565dbab8e17659e9f3
Thomas Off <thomas.off@netzarbeiter.info>
2026-06-30 15:55:31 +0200

    Fix typing errors.

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,11 +2,8 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Carbon;
@@ -29,8 +26,8 @@ use Illuminate\Support\Str;
 #[Hidden(['password', 'two_factor_secret', 'two_factor_recovery_codes', 'remember_token'])]
 class User extends Authenticatable
 {
-    use HasFactory;
     use Notifiable;
+
     /**
      * Get the attributes that should be cast.
      *
